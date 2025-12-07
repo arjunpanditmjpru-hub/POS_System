a2b42c2d1b0517a2a35fe715aba7ef7a378fc17e
Arjun Pandit <arjunpanditmjpru@gmail.com>
2025-12-08 00:18:40 +0530

    commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,7 +80,6 @@ const login = async (req , res , next ) => {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
-            domain: '.onrender.com',
             path: '/',          // VERY IMPORTANT
             maxAge: 1000 * 60 * 60 * 24 * 30
         })
